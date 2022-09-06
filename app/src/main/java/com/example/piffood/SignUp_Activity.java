c1d68300b34d6b0d1d930b5bdf66046264530472
unknown <Administrator@mis02.telho.ltd>
2022-09-06 12:52:25 +0600

    office_init

--- app/src/main/java/com/example/piffood/SignUp_Activity.java
+++ app/src/main/java/com/example/piffood/SignUp_Activity.java
@@ -2,9 +2,15 @@ package com.example.piffood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUp_Activity extends AppCompatActivity {
+
+
+    TextView btnhaveaccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,17 @@ public class SignUp_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         getSupportActionBar().setTitle("Sign Up");
+
+        btnhaveaccount = findViewById(R.id.btnhaveaccount);
+
+        btnhaveaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(SignUp_Activity.this, SingIn_Activity.class);
+                startActivity(myIntent);
+            }
+        });
+
+
     }
 }
