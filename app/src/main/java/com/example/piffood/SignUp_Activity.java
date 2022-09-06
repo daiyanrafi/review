311e4878046b7e364cd7d397ff13949e374e3a18
Daiyan Rafi <daiyanrafi1@gmail.com>
2022-09-07 02:42:28 +0600

    at_night

--- app/src/main/java/com/example/piffood/SignUp_Activity.java
+++ app/src/main/java/com/example/piffood/SignUp_Activity.java
@@ -5,12 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUp_Activity extends AppCompatActivity {
 
 
+    EditText editTextTextPersonName;
+    EditText editTextPhone;
+    EditText editTextTextEmailAddress;
+    EditText editTextTextPassword;
+
+
     TextView btnhaveaccount;
+    Button btnregister;
+    DBHelper DB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +33,52 @@ public class SignUp_Activity extends AppCompatActivity {
 
         btnhaveaccount = findViewById(R.id.btnhaveaccount);
 
+        btnhaveaccount = (Button) findViewById(R.id.btnregister);
+        editTextPhone = (EditText) findViewById(R.id.editTextPhone);
+        editTextTextEmailAddress = (EditText)  findViewById(R.id.editTextTextEmailAddress);
+        editTextTextPassword = (EditText) findViewById(R.id.editTextTextPassword);
+
+        DB = new DBHelper(this);
+
         btnhaveaccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(SignUp_Activity.this, SingIn_Activity.class);
                 startActivity(myIntent);
+
+
+                btnregister.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String user = editTextTextPersonName.getText().toString();
+                        String phone = editTextPhone.getText().toString();
+                        String email = editTextTextEmailAddress.getText().toString();
+                        String pass = editTextTextPassword.getText().toString();
+
+
+                        if(user.equals("")||phone.equals("")||email.equals("")||pass.equals(""))
+                            Toast.makeText(SignUp_Activity.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+                        else {
+                            if (pass.equals(pass)) {
+                                Boolean checkuser = DB.checkusername(user);
+                                if (checkuser == false) {
+                                    Boolean insert = DB.insertData(user, pass);
+                                    if (insert == true) {
+                                        Toast.makeText(SignUp_Activity.this, "Done Successfully", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Toast.makeText(SignUp_Activity.this, "faield", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Toast.makeText(SignUp_Activity.this, "already exist", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(SignUp_Activity.this, "password not matching", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
             }
         });
 
--- app/src/main/java/com/example/piffood/SingIn_Activity.java
+++ app/src/main/java/com/example/piffood/SingIn_Activity.java
@@ -2,9 +2,18 @@ package com.example.piffood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SingIn_Activity extends AppCompatActivity {
+
+    EditText editTextTextEmailAddress, editTextTextPassword;
+    Button btnregister;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +21,38 @@ public class SingIn_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_sing_in);
 
         getSupportActionBar().setTitle("Sign In");
+
+        editTextTextEmailAddress = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        editTextTextPassword = (EditText) findViewById(R.id.editTextTextPassword);
+        btnregister = (Button) findViewById(R.id.btnregister);
+
+        DB = new DBHelper(this);
+
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = editTextTextEmailAddress.getText().toString();
+                String pass = editTextTextPassword.getText().toString();
+
+                if(email.equals("")||pass.equals(""))
+                    Toast.makeText(SingIn_Activity.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+                else {
+                    Boolean checkuserpass = DB.checkusernamepassword(email, pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(SingIn_Activity.this, "Sign in Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        Toast.makeText(SingIn_Activity.this, "Invalid", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+
+            }
+        });
 
     }
 }
